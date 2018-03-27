cf4c1794af3bf54a636512b65ef2a9dd9d9abfe0
Stallixx <iamstallixx@gmail.com>
2018-03-28 02:17:49 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "************************************"
-  ui_print "Pixel 2 Experience v1.9 by Joey Huab"
+  ui_print "Pixel 2 Experience v2.0 by Joey Huab"
   ui_print "************************************"
 }
 
